2ac2b73029e8c0ea11cd014d8073b26cb2daf40c
Ryan <ryan.gross.01@gmail.com>
2022-02-01 21:05:02 -0500

    Recursive search function working

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 states = {}
 target = 0
+lowest_cost = None
 
 
-def run():
+def run(t):
+    global target
+    global lowest_cost
+
+    # Reset globals (just for my internal testing without using files)
+    target = t
+    lowest_cost = None
+
     read_input()
-    print(search(0, 0))
+    search(0, 0)
+
+    if lowest_cost is None:
+        print(-1)
+    else:
+        print(lowest_cost)
 
 
 def read_input():
     global target
     state_array = [2,5] # TODO: pass in state array from file
-    target = 1 # TODO: pass in target water unit from file
+    # TODO: pass in target water unit from file
     set_states(state_array)
 
 
@@ -34,15 +47,44 @@ def set_states(state_array):
 
 
 def search(inf_bucket_volume, steps):
+    global lowest_cost
+    state_to_heuristic_maps = []
+
+    # Calculate heuristic for each sibling "leaf" of a "branch" to see which one to expand
     for state in states:
         # The goal is to reach 0, meaning the difference between our target and actual is 0.
         # Thus the state with the closest value to 0 is chosen as the best path.
         heuristic = (target - (state['value'] + inf_bucket_volume)) * state['cost']
+        state_to_heuristic_maps.append(
+            {
+                "value": state['value'],
+                "heuristic": heuristic,
+                "cost": state['cost']
+            }
+        )
 
-        if heuristic == 0:  # Valid solution
-            return steps + state['cost']
-        elif heuristic < 0:  # Dead end, this bucket is larger than needed volume
-            return -1
+    # Recurse branches until
+    for mapping in sorted(state_to_heuristic_maps, key=lambda k: k['heuristic']):
+        if mapping['heuristic'] < 0:  # Dead end, this bucket is larger than needed volume
+            continue
+        elif mapping['heuristic'] == 0:  # Valid solution
+            cost = steps + mapping['cost']
+            if lowest_cost is None or cost < lowest_cost:
+                lowest_cost = cost
+            continue
+        else:  # Branch a new set of child leaves and continue to approach goal
+            search(inf_bucket_volume + mapping['value'], steps + mapping['cost'])
 
 
-run()
+print("Target: 1")
+run(1)
+print("Target: 2")
+run(2)
+print("Target: 3")
+run(3)
+print("Target: 4")
+run(4)
+print("Target: 5")
+run(5)
+print("Target: 8")
+run(8)
