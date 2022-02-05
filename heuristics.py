3adf8961d5c87abbc414152cd0e79f3d98af71c1
Ryan <ryan.gross.01@gmail.com>
2022-02-05 11:57:31 -0500

    Re-wrote largest bucket heuristic. Test4 is now solved in 16s, but returns 38 instead of 37

--- heuristics.py
+++ heuristics.py
@@ -8,7 +8,31 @@ def simple_heuristic(state: Tuple[int], capacities: Tuple[int], largest_capacity
 
 
 def largest_bucket_first_heuristic(state: Tuple[int], capacities: Tuple[int], largest_capacity: int, target: int) -> int:
-    return math.floor(abs(target - state[-1])/largest_capacity) * 2 - 1
+    delta = target - state[-1]
+    bucket_fills_required_to_repeat_state = 0
+    volumetric_potential_of_current_state = 0
+    capacity_closest_to_delta = capacities[-2]  # Start with 2nd to last bucket since that is biggest
+
+    i = 0
+    for bucket in state[:-1]:  # Loop through all except for target bucket
+        if abs(delta - capacities[i]) < abs(delta - capacity_closest_to_delta):
+            capacity_closest_to_delta = capacities[i]
+
+        if bucket != 0:
+            bucket_fills_required_to_repeat_state += 1
+            volumetric_potential_of_current_state += bucket
+
+    steps_required_to_repeat_state = bucket_fills_required_to_repeat_state * 2  # Fill step + pour step
+
+    # Special case where last step we poured the only full bucket into the target bucket
+    if volumetric_potential_of_current_state == 0:
+        volumetric_potential_of_current_state = capacity_closest_to_delta
+        bucket_fills_required_to_repeat_state = 1
+        steps_required_to_repeat_state = bucket_fills_required_to_repeat_state * 2 + 1  # Extra step required to fill
+
+    h = math.floor(abs(delta)/volumetric_potential_of_current_state * steps_required_to_repeat_state)
+    return h
+
 
 
 def complicated_heuristic(state: Tuple[int], capacities: Tuple[int], largest_capacity: int, target: int) -> int:
