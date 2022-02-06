36b699e7891164cb0d91861ff9b8b3185738fc94
605682 <robles-alcaraz_jesse@bah.com>
2022-02-05 19:07:23 -0500

    All test cases running in reasonable amount of time.

--- bestfirstsearch.py
+++ bestfirstsearch.py
@@ -1,15 +1,17 @@
 from datetime import timedelta
 import itertools
 from operator import itemgetter
-from queue import PriorityQueue
+import heapq
 import sys
 from time import time
 from typing import List, Tuple, Callable
 
-from heuristics import simple_heuristic, complicated_heuristic
+from heuristics import largest_bucket_first_heuristic, largest_bucket_first_heuristic2
 from states import get_child_states
 
-def bfs(*args):
+def bfs(state, capacities, target):
+    if state[-1] == target:
+        return 0
     return 1
 
 def subset_sum(numbers, target):
@@ -38,7 +40,7 @@ def h(state: Tuple[int], capacities: Tuple[int], target: int) -> int:
         # But a smarter approach is to check if any value has such capacity. If not, then
         # we need at least 2 pours: 1 to empty a destination bucket, and another from
         # the infinite bucket to the destination.
-        if all([st - cap != delta for st, cap in zip(state[:-1], capacities[:-1])]):
+        if all([cap - st != delta for cap, st in zip(capacities[:-1], state[:-1])]):
             return 2
         return 1
     if delta > water_available:
@@ -85,7 +87,7 @@ class State:
         if path is None:
             path = []
         self.path = path
-        self.cost = max(1, len(self.path))
+        self.cost = len(self.path)
     
     @property
     def state(self):
@@ -105,52 +107,105 @@ class State:
                 continue
             path = list(self.path)
             path.append(self._state)
-            children.append(State(state, capacities, path))
+            children.append(State(state, self.capacities, path))
         return children
 
 
 class Search:
-    def __init__(self, capacities: Tuple[int], heuristic: Callable) -> None:
-        self.capacities = capacities
+    def __init__(self, capacities: Tuple[int], target: int=None, heuristic: Callable=None) -> None:
+        self.capacities = tuple(sorted([cap for cap in capacities if cap is not None]) + [None])
+        self.target = target
         self.heuristic = heuristic
         self.visited = set()
-        self.pq = PriorityQueue()
-        self.pq.put((0, State((0,)*len(capacities), self.capacities)))
+        self.pq = []
+        heapq.heappush(self.pq, (0, State((0,)*len(capacities), self.capacities)))
+        self.solution_state = None
+        self.time_elapsed = 0
 
-    def search(self, timeout=None, max_iterations=None):
+    def __repr__(self) -> str:
+        return f"<Search{self.solution_state or ''}>"
+        
+    def search(self, heuristic=None, target=None, timeout=None, max_iterations=None):
+        if self.target is None and target is None :
+            raise AttributeError("No target value specified. Pass the `target` argument to the search method.")
+        if self.heuristic is None and heuristic is None:
+            raise AttributeError("No heuristic specified. Pass the `heuristic` argument to the search method.")
+        if heuristic is None:
+            heuristic = self.heuristic
+        target = target or self.target
+        print(f"Capacities: {self.capacities[:-1]}")
+        print(f"Target: {target}")
+        print(f"Heuristic: {heuristic.__name__}")
         solution = -1
         start = time()
         iter_count = 0
-        while not self.pq.empty():
+        # while not self.pq.empty():
+        while self.pq:
             iter_count += 1
             if timeout and time() - start > timeout:
+                self.time_elapsed = time() - start
                 break
             if max_iterations and iter_count > max_iterations:
+                self.time_elapsed = time() - start
                 break
-            state = self.pq.get()[1]
-            # print(f"state: {state}")
+            # state = self.pq.get()[1]
+            state = heapq.heappop(self.pq)[1]
             if is_goal(state, target):
-                print(f"Found: {state.path}")
+                print(f"Found a solution: {state.path + [state.state]}")
                 solution = state.cost
+                self.solution_state = state
+                self.time_elapsed = time() - start
                 break
             for st in state.get_child_states():
                 if not st.state in self.visited:
+                    if is_goal(state, target):
+                        self.check_admissibility(state, heuristic, st.cost)
                     self.visited.add(st.state)
-                    h = self.heuristic(st.state, self.capacities, target)
+                    h = heuristic(st.state, self.capacities, target)
                     cost = h + st.cost
-                    self.pq.put((cost, st))
+                    heapq.heappush(self.pq, (cost, st))
             self.visited.add(state.state)
+        if solution != -1:
+            self.check_admissibility(self.solution_state, heuristic, solution)
         return solution
+    
+    @classmethod
+    def from_file(cls, filename, heuristic=None):
+        capacities, target = parse_file(filename)
+        return cls(capacities=capacities, target=target, heuristic=heuristic)
+
+    def check_admissibility(self, state, heuristic, solution):
+        if heuristic(state.state, self.capacities, self.target) != 0: # Obvious case
+            print("WARNING: Heuristic is not admissible")
+            return
+        for st in state.path:
+            value = heuristic(st, self.capacities, self.target)
+            if value > solution:
+                print("WARNING: Heuristic is not admissible")
+                break
+        else:
+            print("Heuristic seems admissible")
+
 
 
 if __name__ == "__main__":
+    import glob
     file = None
     if len(sys.argv) > 1:
         file = sys.argv[1]
     if file is not None:
-        capacities, target = parse_file(file)
-        s = Search(capacities, simple_heuristic)
         print("Searching...")
-        result = s.search(max_iterations=100000)
+        s = Search.from_file(file)
+        result = s.search(heuristic=largest_bucket_first_heuristic2)
         print(result)
+        print(f"Took: {s.time_elapsed}")
+    else:
+        results = {}
+        for file in glob.glob('*.txt'):
+            s = Search.from_file(file, heuristic=largest_bucket_first_heuristic)
+            result = s.search(timeout=30)
+            results[file] = result
+            print(f"Result: {result}")
+            print(f"Time elapsed: {s.time_elapsed}")
+        print(results)
 
--- heuristics.py
+++ heuristics.py
@@ -1,48 +1,65 @@
 from typing import Tuple
 import itertools
-from operator import itemgetter
+
 
 def simple_heuristic(state: Tuple[int], capacities: Tuple[int], target: int) -> int:
     return abs(state[-1] - target)
 
 
-def largest_bucket_first_heuristic(state: Tuple[int], capacities: Tuple[int], largest_capacity: int, target: int) -> int:
-    return abs(target - state[-1])//largest_capacity * 2 - 1
-
-def complicated_heuristic(state: Tuple[int], target: int) -> int:
-    target_bucket_current = state[-1]
-    delta = target - target_bucket_current
-    if delta == 0: # This means we're at the solution, break early
+def largest_bucket_first_heuristic(state: Tuple[int], capacities: Tuple[int], target: int) -> int:
+    delta = target - state[-1]
+    space_available = [capacity - bucket for capacity, bucket in zip(capacities[:-1], state[:-1])]
+    if delta == 0: # This is a solution state
         return 0
-    if delta > 0: # Case when delta is positive, we need to add to target bucket
-        subset_sums = sorted([seq for i in range(len(state), 0, -1)
-          for seq in itertools.combinations(state, i)
-          if sum(seq) <= delta and not 0 in seq], key=lambda x: (-sum(x), len(x)))
-        if subset_sums: # Means that there is a subset whose sum is <= delta. How should we incorporate how close we are?
-            # index, element = max(enumerate(subset_sums), key=itemgetter(1))
-            element = subset_sums[0] # First element gets us closest
-            return abs(sum(element) - delta) * len(element) + 1
-    return abs(state[-1] - target)
-
-def h(state: Tuple[int], target: int) -> int:
-    global capacities
-    bt = state[-1]
-    delta = target - bt
-    water_available = sum(state[:-1])
-    if delta == 0:
-        return 0
-    if delta > water_available:
-        # The water needed exceeds the amount available in the non-infinite buckets.
-        # then the best case is that a we can pour a bucket to bt, refill a bucket, and pour to bt
-        # return 3
-        if water_available > 0:
-            return target//max(capacities)
-        else:
-            return 3
-    if delta < 0: 
-        # The target bucket is above the value.
-        # Best case is that a bucket has the correct amount missing and we pour the infinite bucket back
+    if delta < 0: # There is more water in the infinite bucket than we need, so we need to pour to other buckets
+        if delta not in space_available: 
+            # There is no single bucket that can accomodate the excess water
+            return 2
         return 1
-    target//max(capacities)
-    raise ValueError(f"Case unaccounted for: {delta}")
+    if delta in state[:-1]: # There is a bucket with exactly the amount needed
+        return 1
+    bucket_fills_required_to_repeat_state = len([bucket for bucket in state[:-1] if bucket != 0])
+    volumetric_potential_of_current_state = sum(state[:-1])
+    capacity_closest_to_delta = min(capacities[:-1], key=lambda x:abs(x-delta))
+    steps_required_to_repeat_state = bucket_fills_required_to_repeat_state * 2 - 1
 
+    # Special case where last step we poured the only full bucket into the target bucket
+    if volumetric_potential_of_current_state == 0:
+        volumetric_potential_of_current_state = capacity_closest_to_delta
+        bucket_fills_required_to_repeat_state = 1
+        steps_required_to_repeat_state = bucket_fills_required_to_repeat_state * 2 # Extra step required to fill
+
+    return max(abs(delta)/volumetric_potential_of_current_state * steps_required_to_repeat_state, 1)
+
+def largest_bucket_first_heuristic2(state: Tuple[int], capacities: Tuple[int], target: int) -> int:
+    delta = target - state[-1]
+    space_available = [capacity - bucket for capacity, bucket in zip(capacities[:-1], state[:-1])]
+    if delta == 0: # This is a solution state
+        return 0
+    # if delta < 0: # There is more water in the infinite bucket than we need, so we need to pour to other buckets
+    #     if delta not in space_available: 
+    #         # There is no single bucket that can accomodate the excess water
+    #         return 2
+    #     return 1
+    if delta in state[:-1]: # There is a bucket with exactly the amount needed
+        return 1
+    bucket_fills_required_to_repeat_state = len([bucket for bucket in state[:-1] if bucket != 0])
+    volumetric_potential_of_current_state = sum(state[:-1])
+    capacity_closest_to_delta = min(capacities[:-1], key=lambda x:abs(x-delta))
+    steps_required_to_repeat_state = bucket_fills_required_to_repeat_state * 2 - 1
+
+    # Special case where last step we poured the only full bucket into the target bucket
+    if volumetric_potential_of_current_state == 0:
+        volumetric_potential_of_current_state = capacity_closest_to_delta
+        bucket_fills_required_to_repeat_state = 1
+        steps_required_to_repeat_state = bucket_fills_required_to_repeat_state * 2 # Extra step required to fill
+
+    h = max(abs(delta)/volumetric_potential_of_current_state * steps_required_to_repeat_state, 1)
+
+    # Case where pouring this bucket into INF would go over our target.
+    # Switch to BFS since our largest bucket first algorithm is no longer useful at this point
+    # This keeps the heuristic admissable
+    if volumetric_potential_of_current_state > abs(delta):
+        h = 1
+
+    return h
